b55bdff964779381dc772141da7a0b35ed471d48
Tiago Eloy Possidonio <105402460+CheweeBR@users.noreply.github.com>
2023-08-29 00:11:46 +0000

    Finalizando lista

--- Estrutura de dados/Exercícios de revisão/Tarefa-de-revisao-01.c
+++ Estrutura de dados/Exercícios de revisão/Tarefa-de-revisao-01.c
@@ -11,16 +11,26 @@ typedef struct Triangulo {
 
 float Area(float a,float b);
 float AreaB(triangulo t);
+void Troca(triangulo a, triangulo b);
 
 int main() {
-    triangulo t;
+    triangulo t, vetor[2], *p;
     float area, areaB;
+    int c;
     printf("\nAltura: ");
     scanf("%f", &t.altura);
     printf("\nBase: ");
     scanf("%f", &t.base);
     area = Area(t.altura, t.base);
     areaB = AreaB(t);
+    printf("A area do triangulo com a funcao `Area`: %2.0f² \nA area do triangulo com a funcao `AreaB`: %2.0f²", area,areaB);
+    for(c=0;c<2;c++){
+        printf("\n%d° Altura: ",c);
+        scanf("%f", &vetor[c].altura);
+        printf("\n%d°Base: ",c);
+        scanf("%f", &vetor[c].base);
+    }
+    Troca(vetor[0],vetor[1]);
     return 0;
 }
 
@@ -32,4 +42,15 @@ float AreaB(triangulo t) {
     float area;
     area = (t.base * t.altura) / 2;
     return area;
+}
+
+void Troca(triangulo a, triangulo b){
+    float aux;
+    aux = a.altura;
+    a.altura = b.altura;
+    b.altura = aux;
+    aux = a.base;
+    a.base = b.base;
+    b.base = aux;
+    printf("Primeiro triangulo:\nBase: %2.0f \nAltura: %2.0f\n\nSegundo triangulo:\nBase: %2.0f \nAltura: %2.0f", a.base,a.altura,b.base,b.altura);
 }
