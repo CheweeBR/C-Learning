0eb4a42b7bec43659df2b734aebd61312a9eafe0
Tiago Eloy Possidonio <105402460+CheweeBR@users.noreply.github.com>
2023-11-21 18:42:34 +0000

    Decremento realizado com sucesso

--- Exercícios/Estrutura de dados/Provas/Avaliação-02.c
+++ Exercícios/Estrutura de dados/Provas/Avaliação-02.c
@@ -41,37 +41,34 @@ void Remover(No **filaAP, No **filaAN, No **filaB, int *cont) {
                 *filaAP = remover->proximo;
                 free(remover);
                 printf("\nRemoção da fila AP bem-sucedida!\n");
-                *cont--;
-            } else {
-                *cont = 6;
+                (*cont)--;
+            } else if (*cont > 2) {
+                if (*filaAN) {
+                    remover = *filaAN;
+                    *filaAN = remover->proximo;
+                    free(remover);
+                    printf("\nRemoção da fila AN bem-sucedida!\n");
+                    (*cont)--;
+                } else if (*cont > 0) {
+                    if (*filaB) {
+                        remover = *filaB;
+                        *filaB = remover->proximo;
+                        free(remover);
+                        printf("\nRemoção da fila B bem-sucedida!\n");
+                        (*cont)--;
+                    } else {
+                        *cont = 10;
+                    }
+                }
             }
-        } else if (*cont > 2) {
-            if (*filaAN) {
-                remover = *filaAN;
-                *filaAN = remover->proximo;
-                free(remover);
-                printf("\nRemoção da fila AN bem-sucedida!\n");
-                *cont--;
-            } else {
-                *cont = 2;
-            }
-        } else {
-            if (*filaB) {
-                remover = *filaB;
-                *filaB = remover->proximo;
-                free(remover);
-                printf("\nRemoção da fila B bem-sucedida!\n");
-                *cont--;
-            } else {
-                *cont = 10;
-            }
-        }
+        } 
     } else {
-        printf("Todas as filas estao vazia.");
+        printf("Todas as filas estao vazias.");
         *cont = 10;
     }
-    printf("%d", *cont);
+    printf("%d\n", *cont);
 }
+
 
 
 void imprimir(No *filaAP, No *filaAN, No *filaB) {
@@ -123,7 +120,8 @@ int main() {
 
     No *fila_AP = NULL, *fila_AN = NULL, *fila_B = NULL;
     Paciente p;
-    int senha, idade, repete = 1, *cont=10;
+    int senha, idade, repete = 1, cont=10;
+    int *pCont = &cont;
     char opcao, grupo;
 
     printf("\n-------------------------------------------------------\n");
@@ -148,7 +146,7 @@ int main() {
                 Inserir(&fila_B, p);
             }
         } else if(opcao == 'r' || opcao == 'R') {
-            Remover(&fila_AP, &fila_AN, &fila_B, &cont);
+            Remover(&fila_AP, &fila_AN, &fila_B, pCont);
         } else if (opcao == 'p' || opcao == 'P') {
             imprimir(fila_AP,fila_AN,fila_B);
         } else {
